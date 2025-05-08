e366eca5fe4f638e6a06d33c2d7caa87048ed187
iguin26 <igordaniel.lopes@gmail.com>
2025-05-08 14:36:39 -0300

    fix: question type in xlsx

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ load_dotenv()
 def open_json(json_path):
     try:
         with open(json_path, 'r', encoding='utf-8') as json_file:
+            print(json_path)
             raw_data: dict = json.load(json_file)
 
 
@@ -33,6 +34,30 @@ def open_json(json_path):
                         
                         data['answer_texto'] = answer.get('titulo', '').replace('\n', ' ')  
                         data['answer_correct'] = answer.get('resposta_correta', '') 
+
+                        data['question_type'] = ''
+
+                        if content['content_type'] == 0:
+                            obj = {
+                                0: "Escolha Única",
+                                1: "Verdadeiro/Falso",
+                                2: "Múltipla Escolha",
+                                3: "Preencha as Lacunas",
+                                4: "Correspondente",
+                                5: "Imagens Correspondentes",
+                                6: "Ordenação",
+                                7: "Dissertativa"
+                            }
+
+                            raw_question_type = content['question_type']
+
+                            question_type = obj[raw_question_type]
+
+                            print(question_type)
+
+
+                            data['question_type'] = question_type
+
 
                         # Only save if 'istemplate' is True and 'datafim' does not exist 
                         datafim = (
@@ -70,12 +95,12 @@ all_rows = []
 xlsx_path =  os.getenv("XLSX_PATH") #path of the csv file
 folder_path = os.getenv("FOLDER_PATH") #path to the folder of the jsons files
 
-# i = 0
+i = 0
 for file in os.listdir(folder_path):
     json_path = os.path.join(folder_path, file)
     open_json(json_path)
-    # if i == 200:
-    #     break
-    # i += 1
+    if i == 0:
+        break
+    i += 1
 
 write_excel(xlsx_path, all_rows)
