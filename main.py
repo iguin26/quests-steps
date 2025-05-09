3114031d5db07bd403a21922b7f08e3ca9737efd
iguin26 <igordaniel.lopes@gmail.com>
2025-05-09 13:45:31 -0300

    print xlsx path

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ load_dotenv()
 def open_json(json_path):
     try:
         with open(json_path, 'r', encoding='utf-8') as json_file:
-            print(json_path)
             raw_data: dict = json.load(json_file)
-
 
             # Only save if 'istemplate' is True and 'datafim' does not exist 
 
@@ -53,9 +51,6 @@ def open_json(json_path):
 
                             question_type = obj[raw_question_type]
 
-                            print(question_type)
-
-
                             data['question_type'] = question_type
 
 
@@ -87,6 +82,7 @@ def write_excel(xlsx_path, all_data):
         df = pd.DataFrame(all_data)
         df.to_excel(xlsx_path, index=False)
         print("Sucessuful operation")
+        print(f"saved in {xlsx_path} ")
     except Exception as e:
         print(f"ERROR IN  XLSX - Error: {e}")
 
@@ -95,12 +91,12 @@ all_rows = []
 xlsx_path =  os.getenv("XLSX_PATH") #path of the csv file
 folder_path = os.getenv("FOLDER_PATH") #path to the folder of the jsons files
 
-i = 0
+# i = 0
 for file in os.listdir(folder_path):
     json_path = os.path.join(folder_path, file)
     open_json(json_path)
-    if i == 0:
-        break
-    i += 1
+    # if i == 0:
+    #     break
+    # i += 1
 
 write_excel(xlsx_path, all_rows)
